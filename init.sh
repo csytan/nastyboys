38332e309a257eab4edfa82f2f98a0abd3540522
Myf Ma <myf@worldtalks.org>
2013-11-13 18:27:22 -0500

    init changes

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 pip install -r requirements.pip
-git submoudles init
-git submoudles update
-python pycurl/setup.py install
+git submodule init
+git submodule update
+cd pycurl
+python setup.py install
+cd ..
